eddb5a639438e01b737e8c4ad4d2c81e0effbc88
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-03-28 12:44:13 +0100

    feat: add AgentSynchronizer into the constructor

--- src/main/java/pcd/ass01/simtrafficbase/CarAgentExtended.java
+++ src/main/java/pcd/ass01/simtrafficbase/CarAgentExtended.java
@@ -1,5 +1,7 @@
 package pcd.ass01.simtrafficbase;
 
+import pcd.ass01.simengineseq.AgentSynchronizer;
+
 import java.util.Optional;
 
 /**
@@ -27,12 +29,13 @@ public class CarAgentExtended extends CarAgent {
 	
 	private int waitingTime;
 	
-	public CarAgentExtended(String id, RoadsEnv env, Road road, 
-					double initialPos, 
-					double acc, 
-					double dec,
-					double vmax) {
-		super(id, env, road, initialPos, acc, dec, vmax);
+	public CarAgentExtended(String id, RoadsEnv env, Road road,
+							double initialPos,
+							double acc,
+							double dec,
+							double vmax,
+							final AgentSynchronizer agentSynchronizer) {
+		super(id, env, road, initialPos, acc, dec, vmax, agentSynchronizer);
 		state = CarAgentState.STOPPED;
 	}
 	
