be65398f70d62f95cc84598a2d248dd4cb1152ac
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-03-28 12:43:01 +0100

    feat: implement the agent synchronizer and implement the run method

--- src/main/java/pcd/ass01/simengineseq/AbstractAgent.java
+++ src/main/java/pcd/ass01/simengineseq/AbstractAgent.java
@@ -5,10 +5,11 @@ package pcd.ass01.simengineseq;
  * Base  class for defining types of agents taking part to the simulation
  * 
  */
-public abstract class AbstractAgent {
+public abstract class AbstractAgent implements Runnable{
 	
-	private String myId;
+	protected String myId;
 	private AbstractEnvironment env;
+	protected volatile boolean flag = false;
 	
 	/**
 	 * Each agent has an identifier
@@ -34,13 +35,12 @@ public abstract class AbstractAgent {
 	 * @param dt - logical time step
 	 */
 	abstract public void step(int dt);
-	
 
-	public String getId() {
-		return myId;
-	}
-	
 	protected AbstractEnvironment getEnv() {
 		return this.env;
+	}
+
+	public String getId(){
+		return this.myId;
 	}
 }
