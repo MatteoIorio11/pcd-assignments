cfa55c1bde73267e82310de08ed8e9fe5470655c
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-17 16:09:28 +0200

    feat: add doc

--- src/main/java/pcd/ass02/server/model/lib/WordCounter.java
+++ src/main/java/pcd/ass02/server/model/lib/WordCounter.java
@@ -5,6 +5,7 @@ public class WordCounter {
     private WordCounter(){}
 
     public static void start(){
+        // take the modality as input and then decide which strategy must be used
         return;
     }
 }
