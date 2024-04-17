326c155d1057c264010dd4471a1e78e6e5df4406
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-17 15:57:29 +0200

    feat: create addFile method

---
+++ src/main/java/pcd/ass02/server/model/lib/event/EventLoopCounter.java
@@ -0,0 +1,2 @@
+package pcd.ass02.server.model.lib.event;public class EventLoopCounter {
+}
--- src/main/java/pcd/ass02/server/model/lib/response/Response.java
+++ src/main/java/pcd/ass02/server/model/lib/response/Response.java
@@ -1,4 +1,25 @@
 package pcd.ass02.server.model.lib.response;
 
+import io.vertx.core.json.JsonObject;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Response {
+    private final Map<String, Integer> results;
+
+    public Response(){
+        this.results = new ConcurrentHashMap<>();
+    }
+
+    public void addFile(final String fileName, final int counter){
+        this.results.put(Objects.requireNonNull(fileName),
+                counter);
+    }
+
+    public JsonObject toJson(){
+
+    }
 }
