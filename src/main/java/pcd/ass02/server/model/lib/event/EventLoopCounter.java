7c604918b9d30633e0ac50e1795ecce67cf8b083
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-17 16:22:49 +0200

    feat: add AbstractVerticle as extend Class

--- src/main/java/pcd/ass02/server/model/lib/event/EventLoopCounter.java
+++ src/main/java/pcd/ass02/server/model/lib/event/EventLoopCounter.java
@@ -1,12 +1,19 @@
 package pcd.ass02.server.model.lib.event;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Promise;
 import pcd.ass02.server.model.lib.WordOccurrence;
 import pcd.ass02.server.model.lib.response.Response;
 
-public class EventLoopCounter implements WordOccurrence {
+public class EventLoopCounter extends AbstractVerticle implements WordOccurrence {
 
+    public void start(){
+
+    }
     @Override
     public Response getWordOccurrences(String path, String word, int depth) {
+        final Promise<Response> promise = Promise.promise();
+
         return null;
     }
 }
