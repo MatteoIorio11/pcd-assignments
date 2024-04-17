491bcec64fe8e154b0eca976cdc016e8a80a8781
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-17 15:50:55 +0200

    refactor: httpServer -> path

--- src/main/java/pcd/ass02/server/model/lib/WordOccurrences.java
+++ src/main/java/pcd/ass02/server/model/lib/WordOccurrences.java
@@ -3,5 +3,5 @@ package pcd.ass02.server.model.lib;
 import pcd.ass02.server.model.lib.response.Response;
 
 public interface WordOccurrences {
-    public Response getWordOccurrences(String httpServer, String word, int depth);
+    public Response getWordOccurrences(String path, String word, int depth);
 }
