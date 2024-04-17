eb36b918e3c0263f59a9fbd661edaab3294f592a
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-17 16:30:19 +0200

    feat: add generic

--- src/main/java/pcd/ass02/server/model/lib/event/EventLoopCounter.java
+++ src/main/java/pcd/ass02/server/model/lib/event/EventLoopCounter.java
@@ -1,19 +1,33 @@
 package pcd.ass02.server.model.lib.event;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Promise;
+import io.vertx.core.Future;
 import pcd.ass02.server.model.lib.WordOccurrence;
 import pcd.ass02.server.model.lib.response.Response;
 
-public class EventLoopCounter extends AbstractVerticle implements WordOccurrence {
+import java.util.Objects;
 
+public class EventLoopCounter extends AbstractVerticle implements WordOccurrence<Future<Response>> {
+
+    private String path;
+    private String word;
+    private int depth;
     public void start(){
 
     }
     @Override
-    public Response getWordOccurrences(String path, String word, int depth) {
-        final Promise<Response> promise = Promise.promise();
-
+    public Future<Response> getWordOccurrences(String path, String word, int depth) {
+        this.path = Objects.requireNonNull(path);
+        this.word = Objects.requireNonNull(word);
+        this.depth = depth;
         return null;
     }
+
+    private Response explore(){
+        final Response response = new Response();
+        if(Objects.nonNull(this.path) && Objects.nonNull(this.word) && this.depth >= 0){
+        }
+        return response;
+    }
+
 }
--- src/main/java/pcd/ass02/server/model/lib/virtual/VirtualCounter.java
+++ src/main/java/pcd/ass02/server/model/lib/virtual/VirtualCounter.java
@@ -3,7 +3,7 @@ package pcd.ass02.server.model.lib.virtual;
 import pcd.ass02.server.model.lib.WordOccurrence;
 import pcd.ass02.server.model.lib.response.Response;
 
-public class VirtualCounter implements WordOccurrence {
+public class VirtualCounter implements WordOccurrence<Response> {
     @Override
     public Response getWordOccurrences(String path, String word, int depth) {
         return null;
