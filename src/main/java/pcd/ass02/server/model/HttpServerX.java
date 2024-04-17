14d5d710685c997af0ca876f95449339edd9aa22
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-17 14:40:16 +0200

    feat: add route and create request handler

--- src/main/java/pcd/ass02/server/model/HttpServerX.java
+++ src/main/java/pcd/ass02/server/model/HttpServerX.java
@@ -5,10 +5,10 @@ import io.vertx.core.http.HttpServer;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
 
-import java.util.Objects;
-
 public class HttpServerX extends AbstractVerticle {
     private final int port;
+    private final static String PATH_DIRECTORY = "directory";
+    private final static String PATH_MODE = "mode";
     public HttpServerX(final int port){
         this.port = port;
     }
@@ -24,8 +24,21 @@ public class HttpServerX extends AbstractVerticle {
     private Router defineRoutes(){
         final Router router = Router.router(vertx);
         router.route().handler(BodyHandler.create());
-
+        router
+                .get("/explore/:" + HttpServerX.PATH_DIRECTORY + "/:" + HttpServerX.PATH_MODE)
+                .respond(context -> {
+                    final String directoryName = context.pathParam(HttpServerX.PATH_DIRECTORY);
+                    final String modality = context.pathParam(HttpServerX.PATH_MODE);
+                    this.log("Request received, directory : " + directoryName + " with the following modality: " + modality);
+                    return context
+                            .response()
+                            .putHeader("Content-Type", "application/json")
+                            .end(directoryName);
+                });
         return router;
     }
 
+    private void log(final String message){
+        System.out.println("[HttpServerX] " + message);
+    }
 }
