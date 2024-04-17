42f0f271e23ea264db6fac95cb0a66d2576eb3b3
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-17 16:03:16 +0200

    feat: add eventLoop counter

--- src/main/java/pcd/ass02/server/model/lib/event/EventLoopCounter.java
+++ src/main/java/pcd/ass02/server/model/lib/event/EventLoopCounter.java
@@ -1,2 +1,12 @@
-package pcd.ass02.server.model.lib.event;public class EventLoopCounter {
+package pcd.ass02.server.model.lib.event;
+
+import pcd.ass02.server.model.lib.WordOccurrences;
+import pcd.ass02.server.model.lib.response.Response;
+
+public class EventLoopCounter implements WordOccurrences {
+
+    @Override
+    public Response getWordOccurrences(String path, String word, int depth) {
+        return null;
+    }
 }
