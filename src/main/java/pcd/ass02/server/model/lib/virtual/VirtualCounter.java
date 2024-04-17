8b0c8440d6b4d1903120f284f6bec26ff5b5507c
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-17 16:02:46 +0200

    chore: add virtual counter

---
+++ src/main/java/pcd/ass02/server/model/lib/virtual/VirtualCounter.java
@@ -0,0 +1,11 @@
+package pcd.ass02.server.model.lib.virtual;
+
+import pcd.ass02.server.model.lib.WordOccurrences;
+import pcd.ass02.server.model.lib.response.Response;
+
+public class VirtualCounter implements WordOccurrences {
+    @Override
+    public Response getWordOccurrences(String path, String word, int depth) {
+        return null;
+    }
+}
