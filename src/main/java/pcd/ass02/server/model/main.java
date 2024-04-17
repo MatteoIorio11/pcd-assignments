f742d8136670dc875156f4f2a57b5e74a6b24406
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-17 14:40:20 +0200

    feat: main

--- src/main/java/pcd/ass02/server/model/main.java
+++ src/main/java/pcd/ass02/server/model/main.java
@@ -1,2 +1,13 @@
-package pcd.ass02.server.model;public class main {
+package pcd.ass02.server.model;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Vertx;
+
+public class main {
+
+    public static void main(String[] args) {
+        Vertx v = Vertx.vertx();
+        final AbstractVerticle h = new HttpServerX(15_000);
+        v.deployVerticle(h);
+    }
 }
