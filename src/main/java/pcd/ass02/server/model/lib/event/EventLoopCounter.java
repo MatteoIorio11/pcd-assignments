017e7084314b5d93e46443ac5fec3763e598ecb1
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-18 10:01:08 +0200

    feat: add EventLoop implementation

--- src/main/java/pcd/ass02/server/model/lib/event/EventLoopCounter.java
+++ src/main/java/pcd/ass02/server/model/lib/event/EventLoopCounter.java
@@ -2,9 +2,13 @@ package pcd.ass02.server.model.lib.event;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
+import io.vertx.core.Promise;
 import pcd.ass02.server.model.lib.WordOccurrence;
+import pcd.ass02.server.model.lib.fs.Directory;
+import pcd.ass02.server.model.lib.fs.Document;
 import pcd.ass02.server.model.lib.response.Response;
 
+import java.io.IOException;
 import java.util.Objects;
 
 public class EventLoopCounter extends AbstractVerticle implements WordOccurrence<Future<Response>> {
@@ -20,14 +24,37 @@ public class EventLoopCounter extends AbstractVerticle implements WordOccurrence
         this.path = Objects.requireNonNull(path);
         this.word = Objects.requireNonNull(word);
         this.depth = depth;
+        return this.explore();
+    }
+
+    private Future<Response> explore(){
+        if(Objects.nonNull(this.path) && Objects.nonNull(this.word) && this.depth >= 0){
+            final Promise<Response> promise = Promise.promise();
+            this.getVertx().setTimer(1000, handler -> {
+                try {
+                    final Response response = new Response();
+                    this.explorePaths(Directory.from(this.path), this.depth, response);
+                    promise.complete(response);
+                } catch (IOException exception) {
+                    promise.fail(exception);
+                }
+            });
+            return promise.future();
+        }
         return null;
     }
 
-    private Response explore(){
-        final Response response = new Response();
-        if(Objects.nonNull(this.path) && Objects.nonNull(this.word) && this.depth >= 0){
+    private void explorePaths(final Directory currDirectory, final int depth, final Response response){
+        if(depth == 0){
+            return;
+        }else{
+            for(final Directory dir : currDirectory.subDirectories()){
+                this.explorePaths(dir, depth - 1, response);
+            }
+            for(final Document document : currDirectory.files()){
+                response.addFile(document.toString(), (int) document.lines().stream().filter(word -> word.equals(this.word)).count());
+            }
         }
-        return response;
     }
 
 }
