e08ca87e1b96fbce9b066b92a85256a8c07868de
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-18 17:16:57 +0200

    feat: add word counter

--- src/main/java/pcd/ass02/server/model/lib/response/Response.java
+++ src/main/java/pcd/ass02/server/model/lib/response/Response.java
@@ -4,12 +4,15 @@ import io.vertx.core.json.JsonObject;
 
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 public class Response {
     private final Map<String, List<String>> results;
+    private final String word;
 
-    public Response(){
+    public Response(final String word){
         this.results = new ConcurrentHashMap<>();
+        this.word = Objects.requireNonNull(word).toLowerCase();
     }
 
     public void addFile(final String fileName, final String line){
@@ -29,5 +32,25 @@ public class Response {
         final JsonObject response = new JsonObject();
         this.results.forEach(response::put);
         return response;
+    }
+
+    public Map<String, Long> count(){
+        return this.results.keySet().stream()
+            .map(fileName -> Map.entry(fileName, this.countWords(fileName)))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    private long countWords(final String fileName){
+        final Optional<Long> result = this.results.get(Objects.requireNonNull(fileName)).parallelStream()
+                .filter(lines -> lines.contains(this.word))
+                .map(lines -> Arrays.stream(lines.split(" "))
+                        .map(String::toLowerCase)
+                        .filter(word -> word.contains(this.word))
+                        .count())
+                .reduce(Long::sum);
+        if (result.isPresent()){
+            return result.get();
+        }
+        return 0;
     }
 }
