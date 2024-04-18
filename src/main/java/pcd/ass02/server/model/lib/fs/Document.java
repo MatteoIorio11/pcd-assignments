63705c7fe45ddd4a6e522b015d2e352cf1abf14c
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-18 10:09:55 +0200

    feat: add name inside the document

--- src/main/java/pcd/ass02/server/model/lib/fs/Document.java
+++ src/main/java/pcd/ass02/server/model/lib/fs/Document.java
@@ -6,16 +6,16 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
 
-public record Document(List<String> lines) {
+public record Document(String documentName, List<String> lines) {
     public static Document from(final String path) {
-        return Document.from(new File(path));
+        return Document.from(path, new File(path));
     }
 
-    public static Document from(final File file) {
+    public static Document from(final String name, final File file) {
         try {
             try (final BufferedReader reader = new BufferedReader(new FileReader(file))) {
                 final var lines = reader.lines().toList();
-                return new Document(lines);
+                return new Document(name, lines);
             }
         } catch (final IOException e) {
             e.printStackTrace(System.err);
