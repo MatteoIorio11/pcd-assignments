396669ba86d7e6b0b212266e67be12f6b9b0f7ed
S-furi <stefano.furi@studio.unibo.it>
2024-04-18 10:23:23 +0200

    fix(fs): retrieve file name from `File.getName()`

--- src/main/java/pcd/ass02/server/model/lib/fs/Directory.java
+++ src/main/java/pcd/ass02/server/model/lib/fs/Directory.java
@@ -7,15 +7,16 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
-public record Directory(List<Document> files, List<Directory> subDirectories) {
+public record Directory(String name, List<Document> files, List<Directory> subDirectories) {
 
     public static Directory from(final String dirname) throws IOException {
         return Directory.from(new File(dirname));
     }
 
-    public static Directory from(final File file){
+    public static Directory from(final File file) {
         final var files = Arrays.stream(Objects.requireNonNull(file.listFiles())).peek(Objects::requireNonNull).toList();
         return new Directory(
+            file.getName(),
             files.stream().filter(f -> !f.isDirectory()).map(Document::from).collect(Collectors.toList()),
             files.stream().filter(File::isDirectory).map(Directory::from).collect(Collectors.toList())
         );
