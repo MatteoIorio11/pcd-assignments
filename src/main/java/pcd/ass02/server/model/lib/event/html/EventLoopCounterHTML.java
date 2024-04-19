d9171da4ce8d47e27705bc7e089c521371a2bf9a
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-19 18:01:19 +0200

    feat: create EventLoopCounterHTML

--- src/main/java/pcd/ass02/server/model/lib/event/html/EventLoopCounterHTML.java
+++ src/main/java/pcd/ass02/server/model/lib/event/html/EventLoopCounterHTML.java
@@ -1,2 +1,14 @@
-package pcd.ass02.server.model.lib.event.html;public class EventLoopCounterHTML {
+package pcd.ass02.server.model.lib.event.html;
+
+import io.vertx.core.Promise;
+import pcd.ass02.server.model.lib.WordOccurrence;
+import pcd.ass02.server.model.lib.response.Response;
+
+import java.util.concurrent.Future;
+
+public class EventLoopCounterHTML implements WordOccurrence<Future<Response>> {
+    @Override
+    public Future<Response> getWordOccurrences(String url, String word, int depth) {
+        return null;
+    }
 }
