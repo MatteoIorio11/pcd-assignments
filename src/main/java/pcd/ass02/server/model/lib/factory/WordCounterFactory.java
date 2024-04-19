62525bbbbe3f40d961031561202fcabcb668f589
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-19 18:20:44 +0200

    feat: add WordCounter factory

---
+++ src/main/java/pcd/ass02/server/model/lib/factory/WordCounterFactory.java
@@ -0,0 +1,20 @@
+package pcd.ass02.server.model.lib.factory;
+
+import io.vertx.core.AbstractVerticle;
+import pcd.ass02.server.model.lib.WordOccurrence;
+import pcd.ass02.server.model.lib.event.fs.EventLoopCounterFS;
+import pcd.ass02.server.model.lib.response.Response;
+import pcd.ass02.server.model.lib.virtual.VirtualCounter;
+
+public class WordCounterFactory {
+
+    private WordCounterFactory(){}
+
+    public static AbstractVerticle fromLoopCounterFS(){
+        return new EventLoopCounterFS();
+    }
+
+    public WordOccurrence<Response> fromVirtualThreadFS(){
+        return new VirtualCounter();
+    }
+}
