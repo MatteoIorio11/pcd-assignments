e87a147d3fe09923e120192adb44809590b2c630
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-19 18:04:46 +0200

    feat: refactor class name

--- src/main/java/pcd/ass02/server/model/lib/event/html/EventLoopCounter.java
+++ src/main/java/pcd/ass02/server/model/lib/event/html/EventLoopCounter.java
@@ -1,12 +1,11 @@
 package pcd.ass02.server.model.lib.event.html;
 
-import io.vertx.core.Promise;
 import pcd.ass02.server.model.lib.WordOccurrence;
 import pcd.ass02.server.model.lib.response.Response;
 
 import java.util.concurrent.Future;
 
-public class EventLoopCounterHTML implements WordOccurrence<Future<Response>> {
+public class EventLoopCounter implements WordOccurrence<Future<Response>> {
     @Override
     public Future<Response> getWordOccurrences(String url, String word, int depth) {
         return null;
