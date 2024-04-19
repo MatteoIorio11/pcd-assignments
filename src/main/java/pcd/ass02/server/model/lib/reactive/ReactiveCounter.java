450705c5d1d3ceb7782da38b059971de60a4916a
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-19 18:01:40 +0200

    feat: create ReactiveCounter

--- src/main/java/pcd/ass02/server/model/lib/reactive/ReactiveCounter.java
+++ src/main/java/pcd/ass02/server/model/lib/reactive/ReactiveCounter.java
@@ -5,7 +5,7 @@ import pcd.ass02.server.model.lib.response.Response;
 
 public class ReactiveCounter implements WordOccurrence {
     @Override
-    public Response getWordOccurrences(String path, String word, int depth) {
+    public Response getWordOccurrences(String url, String word, int depth) {
         return null;
     }
 }
