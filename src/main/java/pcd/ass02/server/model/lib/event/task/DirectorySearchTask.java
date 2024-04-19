f67edb81fc2c67c7deec792ef01fb7a84ee38fcc
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-19 18:01:32 +0200

    feat: add ForkJoin tasks

--- src/main/java/pcd/ass02/server/model/lib/event/task/DirectorySearchTask.java
+++ src/main/java/pcd/ass02/server/model/lib/event/task/DirectorySearchTask.java
@@ -1,2 +1,10 @@
-package pcd.ass02.server.model.lib.event.task;public class DirectorySearchTask {
+package pcd.ass02.server.model.lib.event.task;
+
+import java.util.concurrent.RecursiveTask;
+
+public class DirectorySearchTask extends RecursiveTask<Void> {
+    @Override
+    protected Void compute() {
+        return null;
+    }
 }
--- src/main/java/pcd/ass02/server/model/lib/event/task/DocumentSearchTask.java
+++ src/main/java/pcd/ass02/server/model/lib/event/task/DocumentSearchTask.java
@@ -1,2 +1,27 @@
-package pcd.ass02.server.model.lib.event.task;public class DocumentSearchTask {
+package pcd.ass02.server.model.lib.event.task;
+
+import pcd.ass02.server.model.lib.fs.Document;
+import pcd.ass02.server.model.lib.response.Response;
+
+import java.util.Objects;
+import java.util.concurrent.RecursiveTask;
+
+public class DocumentSearchTask extends RecursiveTask<Void> {
+    private final Document document;
+    private final Response response;
+    private final String word;
+    public DocumentSearchTask(final Response response, final Document document, final String word){
+        super();
+        this.response = Objects.requireNonNull(response);
+        this.document = Objects.requireNonNull(document);
+        this.word = Objects.requireNonNull(word);
+    }
+
+    @Override
+    protected Void compute() {
+        response.addFile(this.document.toString(), this.document.lines().stream()
+                .filter(word -> word.contains(this.word))
+                .toList());
+        return null;
+    }
 }
