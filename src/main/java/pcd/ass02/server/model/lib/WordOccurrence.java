3f5596c114e6e5370290b24b4aa557dc2b4d1558
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-19 18:02:06 +0200

    feat: refactor

--- src/main/java/pcd/ass02/server/model/lib/WordOccurrence.java
+++ src/main/java/pcd/ass02/server/model/lib/WordOccurrence.java
@@ -3,5 +3,5 @@ package pcd.ass02.server.model.lib;
 import pcd.ass02.server.model.lib.response.Response;
 
 public interface WordOccurrence<E> {
-    public E getWordOccurrences(String path, String word, int depth);
+    public E getWordOccurrences(String url, String word, int depth);
 }
