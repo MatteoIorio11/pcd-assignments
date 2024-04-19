91b49719b03b99f586bc67abc53ac304dde885e7
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-19 17:47:46 +0200

    feat: add getParagraphs inside the page

--- src/main/java/pcd/ass02/server/model/lib/html/Page.java
+++ src/main/java/pcd/ass02/server/model/lib/html/Page.java
@@ -2,12 +2,15 @@ package pcd.ass02.server.model.lib.html;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.List;
 import java.util.Objects;
 
 public record Page(String url, Document document) {
+    private final static String PARAGRAPH_TAG = "p";
     public static Page from(final String url) {
         try {
             final Document document = Jsoup.connect(Objects.requireNonNull(url)).get();
@@ -19,7 +22,8 @@ public record Page(String url, Document document) {
     }
 
     public List<String> getParagraphs(){
-
+        final Elements paragraphs = this.document.getElementsByTag(PARAGRAPH_TAG);
+        return paragraphs.stream().map(Element::text).toList();
     }
 
 }
