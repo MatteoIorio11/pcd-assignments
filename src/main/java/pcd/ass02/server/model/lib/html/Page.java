bdc8b0575bc4f8ef059a8263aad34c953b5600ac
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-19 18:00:42 +0200

    feat: add getLinks method and general refactor inside the Page class

--- src/main/java/pcd/ass02/server/model/lib/html/Page.java
+++ src/main/java/pcd/ass02/server/model/lib/html/Page.java
@@ -1,5 +1,6 @@
 package pcd.ass02.server.model.lib.html;
 
+import org.jsoup.HttpStatusException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -11,19 +12,36 @@ import java.util.Objects;
 
 public record Page(String url, Document document) {
     private final static String PARAGRAPH_TAG = "p";
+    private final static String LINK_TAG = "a";
+    private final static String HREF_ATTRIBUTE = "href";
     public static Page from(final String url) {
         try {
             final Document document = Jsoup.connect(Objects.requireNonNull(url)).get();
             return new Page(url, document);
-        }catch (IOException exception){
-            exception.printStackTrace(System.err);
+        } catch (HttpStatusException httpStatusException){
+            System.err.println("[PAGE] The input url: " + url + " does not exists");
+            return null;
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
             return null;
         }
     }
 
     public List<String> getParagraphs(){
-        final Elements paragraphs = this.document.getElementsByTag(PARAGRAPH_TAG);
-        return paragraphs.stream().map(Element::text).toList();
+        final Elements paragraphs = this.document.getElementsByTag(Page.PARAGRAPH_TAG);
+        return paragraphs.stream().map(Element::text)
+                .filter(text -> !text.isEmpty() && !text.isBlank())
+                .toList();
+    }
+
+    public List<Page> getLinks(){
+        final Elements links = this.document.getElementsByTag(Page.LINK_TAG);
+        return links.stream()
+                .map(link ->link.attr(Page.HREF_ATTRIBUTE))
+                .map(href -> this.url + href)
+                .map(Page::from)
+                .filter(Objects::nonNull)
+                .toList();
     }
 
 }
