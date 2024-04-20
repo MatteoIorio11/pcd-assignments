b02706f6099ead33fff36eef6e77b796ae2d3b76
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-20 12:06:26 +0200

    feat: add future logic into the view

--- src/main/java/pcd/ass02/server/view/View.java
+++ src/main/java/pcd/ass02/server/view/View.java
@@ -1,17 +1,24 @@
 package pcd.ass02.server.view;
 
+import io.vertx.core.Future;
 import pcd.ass02.server.controller.Controller;
 import pcd.ass02.server.model.lib.CounterModality;
+import pcd.ass02.server.model.lib.response.Response;
 
 import javax.swing.*;
 import java.awt.*;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 public class View extends JFrame {
     private JTextField urlField, wordField, depthField;
     private JTextArea outputArea;
     private JButton stopButton;
     private JButton startButton;
     private JComboBox comboBox;
+    private boolean inProgress = false;
     private final Controller controller;
     public View(){
         this.controller = new Controller();
@@ -74,11 +81,35 @@ public class View extends JFrame {
         final JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
         startButton = new JButton("Start");
         startButton.addActionListener(e ->  {
-           final String depth = this.depthField.getText();
+           final int depth = Integer.parseInt(this.depthField.getText());
            final String url = this.urlField.getText();
            final String word= this.wordField.getText();
            final CounterModality algorithm = options.get(this.comboBox.getSelectedIndex());
-
+           try {
+               if(!this.inProgress) {
+                   final CompletableFuture<Future<Response>> response = this.controller.startSearch(url, depth, word, algorithm);
+                   this.inProgress = true;
+                   SwingUtilities.invokeLater(() -> {
+                       new Thread(() -> {
+                           try {
+                               response.get().onSuccess(r -> {
+                                   this.outputArea.setText("");
+                                   this.outputArea.setText(r.count().toString());
+                                   this.inProgress = false;
+                               });
+                           } catch (InterruptedException | ExecutionException ex) {
+                               throw new RuntimeException(ex);
+                           }
+                       }).start();
+                   });
+               }else {
+                   System.out.println("Already running please wait.");
+               }
+           }catch (IllegalArgumentException exception){
+                System.err.println(exception.getMessage());
+           } catch (ExecutionException | InterruptedException ex) {
+               throw new RuntimeException(ex);
+           }
         });
         buttonPanel.add(startButton);
 
