b355098b15faea0bec589513a9dc954c3b0f2d5e
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-20 11:47:17 +0200

    feat: general refactor

--- src/main/java/pcd/ass02/server/controller/Controller.java
+++ src/main/java/pcd/ass02/server/controller/Controller.java
@@ -1,8 +1,6 @@
 package pcd.ass02.server.controller;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.Verticle;
-import io.vertx.core.Vertx;
+import io.vertx.core.*;
 import pcd.ass02.server.model.lib.CounterModality;
 import pcd.ass02.server.model.lib.event.html.EventLoopCounter;
 import pcd.ass02.server.model.lib.factory.WordCounterFactory;
@@ -15,41 +13,40 @@ import java.util.Objects;
 
 public class Controller {
 
-
-
     public List<CounterModality> getAlgorithms(){
         return Arrays.stream(CounterModality.values()).toList();
     }
 
-    public Map<String, Long> startSearch(final String url, final int depth, final String word, final CounterModality algorith){
+    public Future<Response> startSearch(final String url, final int depth, final String word, final CounterModality algorith){
         if(Objects.nonNull(url) &&
         Objects.nonNull(word) &&
         Objects.nonNull(algorith) &&
         depth >= 0){
             return switch (algorith)  {
-                case EVENT -> this.startEventLoop(url, depth, word).count();
-                case REACTIVE -> this.startReactive(url, depth, word).count();
-                case VIRTUAL -> this.startVirtual(url, depth, word).count();
+                case EVENT -> this.startEventLoop(url, depth, word);
+                case REACTIVE -> this.startReactive(url, depth, word);
+                case VIRTUAL -> this.startVirtual(url, depth, word);
             };
         }else{
             throw new IllegalArgumentException("One of the input argument is not correct");
         }
     }
 
-    private Response startVirtual(final String url, final int depth, final String word) {
+    private Future<Response> startVirtual(final String url, final int depth, final String word) {
         final var algorithm = WordCounterFactory.fromVirtual();
-        return algorithm.getWordOccurrences(url, word, depth);
+        final Promise<Response> promise = Promise.promise();
+        promise.complete(algorithm.getWordOccurrences(url, word, depth));
+        return promise.future();
     }
 
-    private Response startReactive(final String url, final int depth, final String word) {
+    private Future<Response> startReactive(final String url, final int depth, final String word) {
         return null;
     }
 
-    private Response startEventLoop(final String url, final int depth, final String word){
+    private Future<Response> startEventLoop(final String url, final int depth, final String word){
         final EventLoopCounter algorithm = WordCounterFactory.fromLoopCounter();
         final Vertx vertx = Vertx.vertx();
         vertx.deployVerticle(algorithm);
-        algorithm.getWordOccurrences(url, word, depth);
-        return null;
+        return algorithm.getWordOccurrences(url, word, depth);
     }
 }
