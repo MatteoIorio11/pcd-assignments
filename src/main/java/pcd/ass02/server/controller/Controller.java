d6e3d7435fde99c5e9a62c6c8d64b0c24066b0d9
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-20 11:27:05 +0200

    feat: add start loop counter

--- src/main/java/pcd/ass02/server/controller/Controller.java
+++ src/main/java/pcd/ass02/server/controller/Controller.java
@@ -1,10 +1,16 @@
 package pcd.ass02.server.controller;
 
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Verticle;
+import io.vertx.core.Vertx;
 import pcd.ass02.server.model.lib.CounterModality;
+import pcd.ass02.server.model.lib.event.html.EventLoopCounter;
 import pcd.ass02.server.model.lib.factory.WordCounterFactory;
+import pcd.ass02.server.model.lib.response.Response;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class Controller {
@@ -15,28 +21,35 @@ public class Controller {
         return Arrays.stream(CounterModality.values()).toList();
     }
 
-    public void startSearch(final String url, final int depth, final String word, final CounterModality algorith){
+    public Map<String, Long> startSearch(final String url, final int depth, final String word, final CounterModality algorith){
         if(Objects.nonNull(url) &&
         Objects.nonNull(word) &&
         Objects.nonNull(algorith) &&
         depth >= 0){
-            switch (algorith)  {
-                case EVENT -> this.startEventLoop();
-                case REACTIVE -> this.startReactive();
-                case VIRTUAL -> this.startVirtual();
-            }
+            return switch (algorith)  {
+                case EVENT -> this.startEventLoop(url, depth, word).count();
+                case REACTIVE -> this.startReactive(url, depth, word).count();
+                case VIRTUAL -> this.startVirtual(url, depth, word).count();
+            };
         }else{
             throw new IllegalArgumentException("One of the input argument is not correct");
         }
     }
 
-    private void startVirtual() {
+    private Response startVirtual(final String url, final int depth, final String word) {
+        final var algorithm = WordCounterFactory.fromVirtual();
+        return algorithm.getWordOccurrences(url, word, depth);
     }
 
-    private void startReactive() {
+    private Response startReactive(final String url, final int depth, final String word) {
+        return null;
     }
 
-    private void startEventLoop(){
-        final var algorithm = WordCounterFactory.fromLoopCounter();
+    private Response startEventLoop(final String url, final int depth, final String word){
+        final EventLoopCounter algorithm = WordCounterFactory.fromLoopCounter();
+        final Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(algorithm);
+        algorithm.getWordOccurrences(url, word, depth);
+        return null;
     }
 }
