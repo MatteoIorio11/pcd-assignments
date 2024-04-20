5353dea034ae23604b158991ed492c092df8f6c4
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-20 11:27:13 +0200

    feat: add extension

--- src/main/java/pcd/ass02/server/model/lib/event/html/EventLoopCounter.java
+++ src/main/java/pcd/ass02/server/model/lib/event/html/EventLoopCounter.java
@@ -1,11 +1,12 @@
 package pcd.ass02.server.model.lib.event.html;
 
+import io.vertx.core.AbstractVerticle;
 import pcd.ass02.server.model.lib.WordOccurrence;
 import pcd.ass02.server.model.lib.response.Response;
 
 import java.util.concurrent.Future;
 
-public class EventLoopCounter implements WordOccurrence<Future<Response>> {
+public class EventLoopCounter extends AbstractVerticle implements WordOccurrence<Future<Response>> {
     @Override
     public Future<Response> getWordOccurrences(String url, String word, int depth) {
         return null;
