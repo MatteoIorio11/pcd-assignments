8e878622ba263d3faf697ac497a1dcc6501a55db
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-20 10:07:44 +0200

    feat: create main

--- src/main/java/pcd/ass02/server/view/StartApplication.java
+++ src/main/java/pcd/ass02/server/view/StartApplication.java
@@ -1,2 +1,13 @@
-package pcd.ass02.server.view;public class StartApplication {
+package pcd.ass02.server.view;
+
+import javax.swing.*;
+
+public class StartApplication {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new View().setVisible(true);
+            }
+        });
+    }
 }
