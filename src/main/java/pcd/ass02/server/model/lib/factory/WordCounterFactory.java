770d80360aeef20ad360b2f1bbb5254f249fff95
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-20 10:59:17 +0200

    feat: add virtual thread

--- src/main/java/pcd/ass02/server/model/lib/factory/WordCounterFactory.java
+++ src/main/java/pcd/ass02/server/model/lib/factory/WordCounterFactory.java
@@ -5,6 +5,7 @@ import pcd.ass02.server.model.lib.WordOccurrence;
 import pcd.ass02.server.model.lib.event.fs.EventLoopCounterFS;
 import pcd.ass02.server.model.lib.response.Response;
 import pcd.ass02.server.model.lib.virtual.fs.VirtualCounterFS;
+import pcd.ass02.server.model.lib.virtual.html.VirtualCounter;
 
 public class WordCounterFactory {
 
@@ -12,6 +13,10 @@ public class WordCounterFactory {
 
     public static AbstractVerticle fromLoopCounterFS(){
         return new EventLoopCounterFS();
+    }
+
+    public static WordOccurrence<Response> fromVirtual() {
+        return new VirtualCounter();
     }
 
     public WordOccurrence<Response> fromVirtualThreadFS(){
