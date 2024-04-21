6ee0054aec47a698cfe555b39262858330639767
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-21 11:24:39 +0200

    feat: minor refactor

--- src/main/java/pcd/ass02/server/model/main.java
+++ src/main/java/pcd/ass02/server/model/main.java
@@ -10,28 +10,13 @@ import java.util.Optional;
 public class main {
 
     public static void main(String[] args) {
-        //Vertx v = Vertx.vertx();
-        //final AbstractVerticle h = new HttpServerX(15_000);
-        //v.deployVerticle(h);
-
-        //var r = new Response();
-        //r.addFile("ciao", 0);
-        //r.addFile("zoo", 1);
-        //r.addFile("oo", 2);
-        //System.out.println(r.toJson());
-
-        //final var c = new VirtualCounter();
-        //final Response b = c.getWordOccurrences("./", "java", 2);
-        //System.out.println(b.count());
-        //System.out.println(b.toJson().encodePrettily());
-
-        // Virtual Thread Test
-//        final var vc = WordCounterFactory.fromVirtual();
-//        System.out.println(vc
-//                .getWordOccurrences("https://en.wikipedia.org/wiki/Main_Page", "the", 2)
-//                .count()
-//        );
-
+        /*
+        Some urls:
+            - https://en.wikipedia.org/wiki/Main_Page
+            - https://stackoverflow.com/questions/9119481/how-to-present-a-simple-alert-message-in-java
+            - https://wikileaks.org
+            - https://www.tensorflow.org/?hl=it
+         */
         // EventLoop Test
         final var elvc = WordCounterFactory.fromLoopCounter();
         final Vertx vertx = Vertx.vertx();
