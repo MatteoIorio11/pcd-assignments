f60418b109db5b61a432d82ef485589dc5375045
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-04-24 11:49:56 +0200

    feat: add implementation for a Dummy counter

--- src/main/java/pcd/ass02/server/model/lib/reactive/ReactiveCounter.java
+++ src/main/java/pcd/ass02/server/model/lib/reactive/ReactiveCounter.java
@@ -1,12 +1,39 @@
 package pcd.ass02.server.model.lib.reactive;
 
+import io.reactivex.rxjava3.core.Flowable;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Scheduler;
+import io.reactivex.rxjava3.schedulers.Schedulers;
 import pcd.ass02.server.model.lib.WordOccurrence;
+import pcd.ass02.server.model.lib.html.Page;
 import pcd.ass02.server.model.lib.response.Response;
 
-public class ReactiveCounter implements WordOccurrence {
+import java.util.Collection;
+import java.util.Objects;
+import java.util.Optional;
+
+public class ReactiveCounter implements WordOccurrence<Response> {
+    private Response response;
     @Override
     public Response getWordOccurrences(String url, String word, int depth) {
-        return null;
+        this.response = new Response(Objects.requireNonNull(word));
+        assert depth > 0;
+        final Optional<Page> optPage = Page.from(Objects.requireNonNull(url));
+        optPage.ifPresent(page -> this.explorePages(depth, page, response));
+        return this.response;
+    }
+
+    private void explorePages(final int depth, final Page page, final Response response){
+        if(depth <= 0){
+            return;
+        }
+        Flowable
+                .fromArray(page.getParagraphs())
+                .subscribeOn(Schedulers.computation())
+                .doOnEach(paragraphs -> response.addParagraph(page.url(), paragraphs.getValue()))
+                .subscribe();
+
+        page.getLinks().forEach(link -> this.explorePages(depth - 1, link, response));
     }
 
     @Override
