3944ade5d5c3a0bd0603a4e25a0bdb13a2d724d1
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-15 16:53:39 +0200

    feat: add board initialization with 9 random numbers

--- src/main/java/pcd/ass03/part2/domain/BoardGenerator.java
+++ src/main/java/pcd/ass03/part2/domain/BoardGenerator.java
@@ -1,8 +1,27 @@
 package pcd.ass03.part2.domain;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 public class BoardGenerator {
     private final Board board;
     public BoardGenerator(){
         this.board = new Board();
+        this.initializeBoard();
+    }
+
+    public void initializeBoard(){
+        final Random random = new Random();
+        int positionedNumbers = 0;
+        // add 9 numbers in random positions
+        while (positionedNumbers < 9) {
+            final int x = random.nextInt(0, 10);
+            final int y = random.nextInt(0, 10);
+            final Cell boardCell = new Cell(x, y);
+            if (this.board.isCellEmpty(new Cell(x, y))) {
+                positionedNumbers = positionedNumbers + 1;
+                this.board.putValue(boardCell, positionedNumbers);
+            }
+        }
     }
 }
