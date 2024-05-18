83128d96becccf1e0b05e46ba6b1f7b51f432ba9
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-18 18:05:36 +0200

    feat: create difficulty

---
+++ src/main/java/pcd/ass03/part2/domain/Difficulty.java
@@ -0,0 +1,16 @@
+package pcd.ass03.part2.domain;
+
+public enum Difficulty {
+    EASY(2),
+    NORMAL(4),
+    HARD(6);
+
+    private final int difficulty;
+    Difficulty(final int difficulty){
+        this.difficulty = difficulty;
+    }
+
+    public int getDifficulty() {
+        return difficulty;
+    }
+}
