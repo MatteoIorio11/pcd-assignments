dc4c3a3dd590adcdbff73d75d58b155427a63f80
S-furi <stefano.furi@studio.unibo.it>
2024-05-22 17:00:34 +0200

    chore: add tests

--- src/test/java/pcd/ass03/part2/domain/SudokuSolverTest.java
+++ src/test/java/pcd/ass03/part2/domain/SudokuSolverTest.java
@@ -3,40 +3,51 @@ package pcd.ass03.part2.domain;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class SudokuSolverTest {
     private Board board;
 
     @BeforeEach
     public void setUp() {
-        this.board = new Board();
-        BoardGenerator.initializeBoard(this.board, Difficulty.EASY);
+        this.board = BoardGenerator.initializeBoard(new Board(), Difficulty.EASY);
     }
 
     @Test
     public void testBoardInitialization() {
-        this.board = new Board();
-        BoardGenerator.initializeBoard(this.board, Difficulty.DEBUG);
+        this.board = BoardGenerator.initializeBoard(this.board, Difficulty.DEBUG);
         assertTrue(this.isGameOver());
     }
 
     @Test
     public void testSolver() {
-        SudokuSolver.solve(this.board);
-        assertTrue(this.isGameOver());
+        final var sol = SudokuSolver.solve(this.board);
+        sol.ifPresentOrElse(s -> assertTrue(this.isGameOver(s)), () -> { throw new IllegalStateException(); } );
+    }
+
+    @Test
+    public void testRemoval() {
+        this.board = BoardGenerator.initializeBoard(new Board(), Difficulty.DEBUG);
+        final Cell cell = new Cell(2, 2);
+        assertNotEquals(-1, this.board.getCells().get(cell));
+        this.board.removeValue(cell);
+        assertEquals(-1, this.board.getCells().get(cell));
     }
 
     @Test
     public void testInsertion() {
         final Cell cell = new Cell(2, 2);
+        var oldval = this.board.getCells().get(cell);
         this.board.removeValue(cell);
-        this.board.putValue(cell, 8);
-        assertEquals(8, (int) this.board.getCells().get(cell));
+        this.board.putValue(cell, oldval);
+        assertEquals(oldval, (int) this.board.getCells().get(cell));
     }
 
     private boolean isGameOver() {
-        return this.board.getCells().values().stream().allMatch(t -> t != -1);
+        return this.isGameOver(this.board);
+    }
+
+    private boolean isGameOver(final Board board) {
+        return board.getCells().values().stream().allMatch(t -> t != -1);
     }
 }
