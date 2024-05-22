98906702703920ff034fef38897d0e4106711830
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-22 12:29:17 +0200

    refactor: change hardcoded values with static ones

--- src/main/java/pcd/ass03/part2/domain/mom/RemoteBroker.java
+++ src/main/java/pcd/ass03/part2/domain/mom/RemoteBroker.java
@@ -1,5 +1,6 @@
 package pcd.ass03.part2.domain.mom;
 
+import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
@@ -11,7 +12,6 @@ import java.util.concurrent.TimeoutException;
 
 public class RemoteBroker {
     private static final ConnectionFactory CONNECTION_FACTORY = new ConnectionFactory();
-    private static final String QUEUE_NAME= "game";
     private static final String HOST = "rogmlmgd";
     private static final String VHOST = "rogmlmgd";
     private static final String PASSWORD = "sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq";
@@ -22,11 +22,16 @@ public class RemoteBroker {
 
     private RemoteBroker(){}
     private static void setConnectionInforomation() throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
-        CONNECTION_FACTORY.setHost("rogmlmgd");
-        CONNECTION_FACTORY.setVirtualHost("rogmlmgd");
-        CONNECTION_FACTORY.setPassword("sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq");
-        CONNECTION_FACTORY.setPort(5672);
-        CONNECTION_FACTORY.setUri("amqp://rogmlmgd:sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq@rat.rmq2.cloudamqp.com:5672/rogmlmgd");
+        //CONNECTION_FACTORY.setHost("rogmlmgd");
+        CONNECTION_FACTORY.setHost(RemoteBroker.HOST);
+        CONNECTION_FACTORY.setVirtualHost(RemoteBroker.VHOST);
+        //CONNECTION_FACTORY.setVirtualHost("rogmlmgd");
+        //CONNECTION_FACTORY.setPassword("sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq");
+        CONNECTION_FACTORY.setPassword(RemoteBroker.PASSWORD);
+        CONNECTION_FACTORY.setPort(RemoteBroker.PORT);
+        //CONNECTION_FACTORY.setPort(5672);
+        //CONNECTION_FACTORY.setUri("amqp://rogmlmgd:sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq@rat.rmq2.cloudamqp.com:5672/rogmlmgd");
+        CONNECTION_FACTORY.setUri(RemoteBroker.URI);
         CONNECTION_FACTORY.setConnectionTimeout(30000);
     }
 
@@ -34,5 +39,9 @@ public class RemoteBroker {
         setConnectionInforomation();
         return CONNECTION_FACTORY.newConnection();
     }
-    
+
+    public static Channel createChannel() throws IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
+        return RemoteBroker.createConnection().createChannel();
+    }
+
 }
