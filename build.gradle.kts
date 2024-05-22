3a24a96262994e8a58bf3f62ca75fff75ad33f73
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-22 09:44:40 +0200

    feat: add jackson

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,6 +31,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     implementation("com.rabbitmq:amqp-client:5.21.0")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.0")
 }
 
 tasks.test {
