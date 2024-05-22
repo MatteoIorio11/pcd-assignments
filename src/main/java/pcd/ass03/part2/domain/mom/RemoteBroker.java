3b2ff1d0de8fe39e4e30a4741dc60da946de0da9
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-22 12:00:23 +0200

    feat: add createConnection factory

--- src/main/java/pcd/ass03/part2/domain/mom/RemoteBroker.java
+++ src/main/java/pcd/ass03/part2/domain/mom/RemoteBroker.java
@@ -1,13 +1,16 @@
 package pcd.ass03.part2.domain.mom;
 
+import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.TimeoutException;
 
 public class RemoteBroker {
-    private final ConnectionFactory factory;
+    private static final ConnectionFactory CONNECTION_FACTORY = new ConnectionFactory();
     private static final String QUEUE_NAME= "game";
     private static final String HOST = "rogmlmgd";
     private static final String VHOST = "rogmlmgd";
@@ -15,18 +18,21 @@ public class RemoteBroker {
     private static final String SERVER = "rat.rmq2.cloudamqp.com";
     private static final int PORT = 5672;
     private static final String URI = "amqp://"+RemoteBroker.HOST+":"+RemoteBroker.PASSWORD+"@"+RemoteBroker.SERVER+":"+RemoteBroker.PORT+"/"+ RemoteBroker.VHOST;
-    public RemoteBroker() throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
-        this.factory = new ConnectionFactory();
-        this.factory.setHost("rogmlmgd");
-        this.factory.setVirtualHost("rogmlmgd");
-        this.factory.setPassword("sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq");
-        this.factory.setPort(5672);
-        this.factory.setUri("amqp://rogmlmgd:sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq@rat.rmq2.cloudamqp.com:5672/rogmlmgd");
-        this.factory.setConnectionTimeout(30000);
+
+
+    private RemoteBroker(){}
+    private static void setConnectionInforomation() throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
+        CONNECTION_FACTORY.setHost("rogmlmgd");
+        CONNECTION_FACTORY.setVirtualHost("rogmlmgd");
+        CONNECTION_FACTORY.setPassword("sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq");
+        CONNECTION_FACTORY.setPort(5672);
+        CONNECTION_FACTORY.setUri("amqp://rogmlmgd:sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq@rat.rmq2.cloudamqp.com:5672/rogmlmgd");
+        CONNECTION_FACTORY.setConnectionTimeout(30000);
     }
 
-    private void configureQueue(){
-        
+    public static Connection createConnection() throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException, IOException, TimeoutException {
+        setConnectionInforomation();
+        return CONNECTION_FACTORY.newConnection();
     }
-
+    
 }
