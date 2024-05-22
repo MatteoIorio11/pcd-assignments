be342571c397dcbfb8595b88d434063f3861f625
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-22 10:44:58 +0200

    feat: add constructor

--- src/main/java/pcd/ass03/part2/domain/mom/MOMPlayer.java
+++ src/main/java/pcd/ass03/part2/domain/mom/MOMPlayer.java
@@ -1,4 +1,13 @@
 package pcd.ass03.part2.domain.mom;
 
+import pcd.ass03.part2.domain.Board;
+
+import java.util.Objects;
+
 public class MOMPlayer {
+    final Board board;
+
+    public MOMPlayer(final Board board){
+        this.board = Objects.requireNonNull(board);
+    }
 }
