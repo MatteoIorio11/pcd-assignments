f3d47c4f13885274df541914610d82d1bb579812
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-22 14:16:07 +0200

    refactor: remove all the hard coded values, rename the set method

--- src/main/java/pcd/ass03/part2/domain/mom/RemoteBroker.java
+++ src/main/java/pcd/ass03/part2/domain/mom/RemoteBroker.java
@@ -1,6 +1,5 @@
 package pcd.ass03.part2.domain.mom;
 
-import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
@@ -21,27 +20,17 @@ public class RemoteBroker {
 
 
     private RemoteBroker(){}
-    private static void setConnectionInforomation() throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
-        //CONNECTION_FACTORY.setHost("rogmlmgd");
+    private static void setConnectionInformation() throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
         CONNECTION_FACTORY.setHost(RemoteBroker.HOST);
         CONNECTION_FACTORY.setVirtualHost(RemoteBroker.VHOST);
-        //CONNECTION_FACTORY.setVirtualHost("rogmlmgd");
-        //CONNECTION_FACTORY.setPassword("sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq");
         CONNECTION_FACTORY.setPassword(RemoteBroker.PASSWORD);
         CONNECTION_FACTORY.setPort(RemoteBroker.PORT);
-        //CONNECTION_FACTORY.setPort(5672);
-        //CONNECTION_FACTORY.setUri("amqp://rogmlmgd:sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq@rat.rmq2.cloudamqp.com:5672/rogmlmgd");
         CONNECTION_FACTORY.setUri(RemoteBroker.URI);
         CONNECTION_FACTORY.setConnectionTimeout(30000);
     }
 
     public static Connection createConnection() throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException, IOException, TimeoutException {
-        setConnectionInforomation();
+        setConnectionInformation();
         return CONNECTION_FACTORY.newConnection();
     }
-
-    public static Channel createChannel() throws IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
-        return RemoteBroker.createConnection().createChannel();
-    }
-
 }
