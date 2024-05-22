c5faa115eb4f31697e359e3c03061aceed162243
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-22 11:20:12 +0200

    feat: add static variables

--- src/main/java/pcd/ass03/part2/domain/mom/RemoteBroker.java
+++ src/main/java/pcd/ass03/part2/domain/mom/RemoteBroker.java
@@ -9,6 +9,12 @@ import java.security.NoSuchAlgorithmException;
 public class RemoteBroker {
     private final ConnectionFactory factory;
     private static final String QUEUE_NAME= "game";
+    private static final String HOST = "rogmlmgd";
+    private static final String VHOST = "rogmlmgd";
+    private static final String PASSWORD = "sjdLZ_RORmQtT6VhpjzhiYa3pVIcOgqq";
+    private static final String SERVER = "rat.rmq2.cloudamqp.com";
+    private static final int PORT = 5672;
+    private static final String URI = "amqp://"+RemoteBroker.HOST+":"+RemoteBroker.PASSWORD+"@"+RemoteBroker.SERVER+":"+RemoteBroker.PORT+"/"+ RemoteBroker.VHOST;
     public RemoteBroker() throws URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
         this.factory = new ConnectionFactory();
         this.factory.setHost("rogmlmgd");
