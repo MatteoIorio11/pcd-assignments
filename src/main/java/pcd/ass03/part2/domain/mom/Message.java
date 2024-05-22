9df129ea29d04bca72789ba66784e8bdd0b0da0c
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-22 10:26:06 +0200

    feat: add Pair record

--- src/main/java/pcd/ass03/part2/domain/mom/Message.java
+++ src/main/java/pcd/ass03/part2/domain/mom/Message.java
@@ -1,6 +1,18 @@
 package pcd.ass03.part2.domain.mom;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import pcd.ass03.part2.domain.Cell;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-public record Message(Cell cell, int value) {
+
+record Pair(Cell x, int y){
+}
+
+public record Message() {
+    public static String putValue(final Cell cell, final int value) throws JsonProcessingException {
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final String jsonString = objectMapper.writeValueAsString(pair);
+        System.out.println(jsonString);
+        return "";
+    }
 }
