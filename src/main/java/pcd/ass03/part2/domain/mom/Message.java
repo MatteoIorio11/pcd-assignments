a845f196ac61049c258da60742a99ece4aad912d
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-22 10:34:10 +0200

    feat: add Pair creation with a static method, add getMove inside the message

--- src/main/java/pcd/ass03/part2/domain/mom/Message.java
+++ src/main/java/pcd/ass03/part2/domain/mom/Message.java
@@ -7,15 +7,20 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.Objects;
 
 
-record Pair(Cell cell, int value){
-    public static Pair createPair(final Cell cell, final int value){
-        return new Pair(Objects.requireNonNull(cell), value);
-    }
-}
-
 public record Message() {
-    public static String putValue(final Cell cell, final int value) throws JsonProcessingException {
+    public record Pair(Cell cell, int value){
+        public static Pair createPair(final Cell cell, final int value){
+            return new Pair(Objects.requireNonNull(cell), value);
+        }
+    }
+
+    public static String sendMove(final Cell cell, final int value) throws JsonProcessingException {
         final ObjectMapper objectMapper = new ObjectMapper();
         return objectMapper.writeValueAsString(Pair.createPair(cell, value));
+    }
+
+    public static Pair getMove(final String message) throws JsonProcessingException {
+        final ObjectMapper mapper = new ObjectMapper();
+        return mapper.readValue(message, Pair.class);
     }
 }
