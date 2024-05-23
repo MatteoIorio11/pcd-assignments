b111b743a62ccd380267897238fcfd2b8c03e3ef
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-23 12:19:13 +0200

    feat: add equals

--- src/main/java/pcd/ass03/part2/domain/Cell.java
+++ src/main/java/pcd/ass03/part2/domain/Cell.java
@@ -1,7 +1,22 @@
 package pcd.ass03.part2.domain;
 
+import java.util.Objects;
+
 public record Cell(int i, int j) {
     public static Cell copyOf(final Cell cell) {
         return new Cell(cell.i, cell.j);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell cell = (Cell) o;
+        return i == cell.i && j == cell.j;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(i, j);
     }
 }
