6068e898cf01e41090790804b3ffe61bdbb1889d
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-23 09:54:31 +0200

    feat: add difficulty

--- src/main/java/pcd/ass03/part2/domain/mom/MOMPlayer.java
+++ src/main/java/pcd/ass03/part2/domain/mom/MOMPlayer.java
@@ -1,7 +1,9 @@
 package pcd.ass03.part2.domain.mom;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import pcd.ass03.part2.domain.Board;
 import pcd.ass03.part2.domain.Cell;
+import pcd.ass03.part2.domain.Difficulty;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -13,14 +15,14 @@ import java.util.concurrent.TimeoutException;
 public class MOMPlayer {
     private final Board board;
     private final Middleware middleware;
-    public MOMPlayer(final Board board) throws IOException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException, TimeoutException {
+    public MOMPlayer(final Board board, final Difficulty difficulty) throws IOException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException, TimeoutException {
         this.board = Objects.requireNonNull(board);
-        this.middleware = new Middleware();
+        this.middleware = new Middleware(difficulty);
     }
 
-    public void putValue(final Cell cell, final int value){
+    public void putValue(final Cell cell, final int value) throws IOException {
         this.board.putValue(cell, value);
-
+        this.middleware.marshall(Message.marshall(cell, value));
     }
 
     public void removeValue(final Cell cell){
