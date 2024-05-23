54bc3058ec1429104f7ad13cf8042f23cfe887df
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-23 11:38:47 +0200

    feat: add registry for the RMI

--- src/main/java/pcd/ass03/part2/domain/rmi/RMIMiddleware.java
+++ src/main/java/pcd/ass03/part2/domain/rmi/RMIMiddleware.java
@@ -3,8 +3,19 @@ package pcd.ass03.part2.domain.rmi;
 import pcd.ass03.part2.domain.Difficulty;
 import pcd.ass03.part2.logics.Controller;
 
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Optional;
+
 public class RMIMiddleware extends Controller {
+    private Optional<Registry> registry = Optional.empty();
+    private static final String HOST = "localhost";
     public RMIMiddleware(final Difficulty difficulty) {
         super(difficulty);
+        try {
+            this.registry = Optional.of(LocateRegistry.getRegistry(HOST));
+        }catch (Exception e){
+            //
+        }
     }
 }
