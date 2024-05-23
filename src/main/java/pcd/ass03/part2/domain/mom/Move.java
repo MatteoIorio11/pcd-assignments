03e96e9ca06c0575a5a5edbbd7b3c63348e15e82
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-23 09:45:30 +0200

    feat: create a new record for the move

--- src/main/java/pcd/ass03/part2/domain/mom/Move.java
+++ src/main/java/pcd/ass03/part2/domain/mom/Move.java
@@ -1,2 +1,19 @@
-package pcd.ass03.part2.domain.mom;public record Move() {
+package pcd.ass03.part2.domain.mom;
+
+import pcd.ass03.part2.domain.Cell;
+
+import java.util.Objects;
+
+public record Move(Cell cell, int value) {
+
+    /**
+     * Create the move
+     *
+     * @param cell:  input cell of the move
+     * @param value: input value of the move
+     * @return the move to apply
+     */
+    public static Move createMove(final Cell cell, final int value) {
+        return new Move(Objects.requireNonNull(cell), value);
+    }
 }
