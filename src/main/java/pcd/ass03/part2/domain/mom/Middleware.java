303e310b2c03e764572f7fb5fc864599943afa44
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-23 09:45:23 +0200

    feat: create a queue for the incoming messages

--- src/main/java/pcd/ass03/part2/domain/mom/Middleware.java
+++ src/main/java/pcd/ass03/part2/domain/mom/Middleware.java
@@ -3,12 +3,17 @@ package pcd.ass03.part2.domain.mom;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.DeliverCallback;
+import pcd.ass03.part2.domain.Board;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.concurrent.TimeoutException;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 
 public class Middleware {
     private final Connection connection;
@@ -17,15 +22,18 @@ public class Middleware {
     private static final String EXHANGE_NAME = "game";
     private static final String ROUTING_KEY = "move";
     private static final String TYPE = "direct";
+    private final Queue<Move> incomingMoves;
 
-    public Middleware(final Runnable onReceive) throws IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
+    public Middleware(final BiConsumer<Board, Move> onReceive) throws IOException, TimeoutException, URISyntaxException, NoSuchAlgorithmException, KeyManagementException {
         this.connection = RemoteBroker.createConnection();
         this.channel = this.connection.createChannel();
+        this.incomingMoves = new LinkedList<>();
         this.setChannel();
         this.deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
-            onReceive.run();
+            incomingMoves.add(Message.getMove(message));
             System.out.println(" [x] Received '" + delivery.getEnvelope().getRoutingKey() + "':'" + message + "'");
+
         };
         String queueName = channel.queueDeclare().getQueue();
         this.channel.basicConsume(queueName, true, deliverCallback, consumerTag -> { });
@@ -47,6 +55,5 @@ public class Middleware {
         }catch (Exception ex){
             //
         }
-
     }
 }
