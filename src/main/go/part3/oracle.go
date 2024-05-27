0a262473824a4ef59f3561d197368fc3ae709a5d
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-27 16:40:47 +0200

    feat: add function for creating channels

--- src/main/go/part3/oracle.go
+++ src/main/go/part3/oracle.go
@@ -8,6 +8,14 @@ type Oracle struct {
 
 func (o Oracle) handleMessage(v int)
 
-func spawnOracle(maxValue int) {
+func createChannels[X any](size int, channelType X) []chan X {
+	channels := make([]chan X, size)
+	for i := 0; i < size; i++ {
+		channels[i] = make(chan X)
+	}
+	return channels
+}
+
+func spawnOracle(maxValue int, players int) {
 	oracle := Oracle{value: 10, inputChannel: nil, outputChannel: nil}
 }
