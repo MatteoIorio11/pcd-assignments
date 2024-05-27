39a714bb63b8e03baf7f1ab9c02c70fd2eb061e1
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-27 14:48:01 +0200

    chore: init messages

--- src/main/go/part3/main.go
+++ src/main/go/part3/main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+type Hint int
+
+const (
+	GREATER Hint = iota
+	SMALLER Hint = iota
+)
+
+type Message struct {
+	guessedNumber int // message sent by the player
+	playerID      int
+}
+
+// Message sent from the Oracle to the player
+type Response struct {
+	status   bool // true won, false lost
+	hintV    Hint
+	playerID int
+}
+
+// The oracle has X channels, one for each player. The playerID is the index for the channel
+// The player has only one channel
+
 func main() {
-	fmt.Println("Hello World");
+	fmt.Println("Hello World")
 }
