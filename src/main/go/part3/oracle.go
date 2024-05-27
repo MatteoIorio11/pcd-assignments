bf7f3bb702efa3cff65c5ea504e6f7bf5a5f5e98
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-05-27 16:14:01 +0200

    feat: add oracle struct

--- src/main/go/part3/oracle.go
+++ src/main/go/part3/oracle.go
@@ -1,1 +1,13 @@
 package main
+
+type Oracle struct {
+	value         int
+	inputChannel  []chan Message
+	outputChannel []chan Response
+}
+
+func (o Oracle) handleMessage(v int)
+
+func spawnOracle(maxValue int) {
+	oracle := Oracle{value: 10, inputChannel: nil, outputChannel: nil}
+}
